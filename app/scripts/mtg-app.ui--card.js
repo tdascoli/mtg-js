b577387a20d9073ce3926039ffef0576918ae9a1
tdascoli <thomas@dasco.li>
2016-10-28 14:36:48 +0200

    new playground

--- app/scripts/mtg-app.ui--card.js
+++ app/scripts/mtg-app.ui--card.js
@@ -1,6 +1,5 @@
 $( document ).ready(function() {
-
-    $('.playground .mtg-card').click(function(){
+    $('.playground__player .mtg-card').click(function(){
         if (!$(this).hasClass('mtg-card--no-tap')) {
             $(this).toggleClass('mtg-card--tapped');
         }
