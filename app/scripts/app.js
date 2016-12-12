6aead9176899062c15e9fc52fe269f2106efa8aa
tdascoli <thomas@dasco.li>
2016-12-12 22:38:53 +0100

    fix

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -408,8 +408,9 @@ var app = angular.module('mtgJsApp', [
   });
 
   // TODO REMOVE WHEN RESOLVED --> UI-ROUTER
+  /*
   app.config(['$qProvider', function ($qProvider) {
     $qProvider.errorOnUnhandledRejections(false);
   }]);
-
+  */
 }());
